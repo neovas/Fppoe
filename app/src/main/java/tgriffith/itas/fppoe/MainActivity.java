10ab1bf33798d5799e54e6e3a256fb097d7db7bb
neovas <thomas-griffith@live.com>
2020-03-10 09:28:33 -0700

    Initial commit - Linked with android studio. Basic activity.

---
+++ app/src/main/java/tgriffith/itas/fppoe/MainActivity.java
@@ -0,0 +1,14 @@
+package tgriffith.itas.fppoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
